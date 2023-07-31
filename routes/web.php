49a902bfceeda57776948dfd6e34fef0469f5141
Ali.t.nazari <alitabatabaee20@gmail.com>
2023-07-31 20:50:14 +0430

    add admin panel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+Route::get('/admin', [AdminController::class, 'index']);
